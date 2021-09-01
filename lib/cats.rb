becfda55acbc79d40ef916975bd43d2a9badb5bf
Anna Wilson <ajoyna01@gmail.com>
2021-09-01 10:50:56 -0500

    next

--- lib/cats.rb
+++ lib/cats.rb
@@ -1,3 +1,14 @@
 class Cats
+  attr_accessor :name :color :weight :danger_level
+
+  def initialize(name, color, weight, danger_level)
+    @name =name
+    @color = color
+    @weight = weight
+    @danger_level = danger_level
+  end
+
+  def 
+
 
 end
--- lib/cats_cli.rb
+++ lib/cats_cli.rb
@@ -1,3 +1,5 @@
+CATS =[]
+
 def start_cli
     puts "Hi! Welcome to the Feral and Unloved Cats CLI"
     main_menu
@@ -6,13 +8,17 @@ def start_cli
   end
   
   def main_menu
-    puts "Here's what you can do:".cyan
+      puts "What would you like to do? Type the number that matches your choice or 'exit' to leave the program".cyan
+      print_options
+  end
+    
+  def print_options
+    puts "Here's what you can do. Type:".cyan
     puts "  1. Add Cat".cyan
     puts "  2. List Cats".cyan
     puts "  3. Return Cat".cyan
     puts "  4. Donate to Feral Cats".cyan
     puts "  5. Refer a Cat".cyan
-    # Add other options like "edit thing, delete thing, etc..."
     puts "  exit".cyan
     puts "Type the number corresponding to your choice, or 'exit' to leave the program".cyan
   end
@@ -21,9 +27,55 @@ def start_cli
     input = gets.chomp
     while input != "exit" do
       if input =="1"
-        add_cat
-      else
+        cat = add_cat
+        cat.print
+      elsif input == "2"
+        CATS.each do
+          cat.print
+        end
+      elsif input == "3"
+        puts "We are sorry you are not satisfied with your cat!"
+        return_cat 
+      elsif input == "4"
+        donate_to_cats
+      elsif input == "5"
+        refer_cat
+      elsif inoput == "debug"
+      binding.pry
+      else puts "Whoops! I didn't understand your choice".red
     end
-    # add conditional logic for what to do when a user types a certain input
+      main_menu
+      input = gets.chomp
+    end
+    end
+  
+  def add_cat
+    print "What's the cat's name?".green
+    name = gets.chomp
+    print "What's the cat's color?".green
+    color = gets.chomp 
+    print "How much does the cat weight?".green
+    weight = gets.chomp
+    print "How dangerous is the cat?".green
+    danger_level = gets.chomp
+    cat = (
+      name: name,
+      color: color,
+      weight: weight,
+      danger_level: danger_level
+    ) 
+    CATS << cat
+    cat
   end
+  def return_cat
+    print "Sorry you're not happy with your cat. Please choose the cat you wish to return:"
+      CATS.each.with_index(1) do |cat, index|
+        puts "#{index}. #{cat.name}"
+      end
+      input = gets.chomp
+      input_to_index = input_to_index - 1
+      cat_to_return = CATS[input_to_index]
+      CATS << cat_to_return
+
+end
 
