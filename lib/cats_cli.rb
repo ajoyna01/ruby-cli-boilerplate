9132e21633b8f52fa6e546d3c0c07f6633a96efb
Anna Wilson <ajoyna01@gmail.com>
2021-09-01 09:42:58 -0500

    Switching

--- lib/cats_cli.rb
+++ lib/cats_cli.rb
@@ -1,14 +1,17 @@
 def start_cli
-    puts "Hi! Welcome to the Things CLI"
+    puts "Hi! Welcome to the Feral and Unloved Cats CLI"
     main_menu
     handle_user_choice
-    puts "Thanks for using the Things CLI!"
+    puts "Thanks for using the Feral and Unloved CLI!"
   end
   
   def main_menu
     puts "Here's what you can do:".cyan
-    puts "  1. Add Thing".cyan
-    puts "  2. List Things".cyan
+    puts "  1. Add Cat".cyan
+    puts "  2. List Cats".cyan
+    puts "  3. Return Cat".cyan
+    puts "  4. Donate to Feral Cats".cyan
+    puts "  5. Refer a Cat".cyan
     # Add other options like "edit thing, delete thing, etc..."
     puts "  exit".cyan
     puts "Type the number corresponding to your choice, or 'exit' to leave the program".cyan
@@ -16,7 +19,11 @@ def start_cli
 
   def handle_user_choice
     input = gets.chomp
-    
+    while input != "exit" do
+      if input =="1"
+        add_cat
+      else
+    end
     # add conditional logic for what to do when a user types a certain input
   end
 
